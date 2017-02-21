57a5a932f29b8d99cf726953cd90928281daf473
Pekka Niiranen <nihilpekka@gmail.com>
2017-02-21 10:32:16 +0200

    added devtoos line

--- EteneeFront/main.js
+++ EteneeFront/main.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux'
 import App from './App.jsx'
 
 //let store = createStore()
+//let store = createStore(itemApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 let rootElement = document.getElementById('app')
 
