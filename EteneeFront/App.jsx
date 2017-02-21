de9dec40a216b2b93835c0d0d264873bbf53d961
Pekka Niiranen <nihilpekka@gmail.com>
2017-02-21 09:33:45 +0200

    added material design

--- EteneeFront/App.jsx
+++ EteneeFront/App.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import Test from './components/test.jsx'
 
@@ -18,11 +20,11 @@ class App extends Component {
    }
 }
 
-function select(state) {
+/*function select(state) {
   console.log('state active');
    return {
       //
    }
-}
+}*/
 
 export default App;
