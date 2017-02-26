8fc1e276559a439cc3a425672b4da46477db14f3
Pekka Niiranen <nihilpekka@gmail.com>
2017-02-26 20:03:57 +0200

    added headlines for components

--- EteneeFront/components/LuKStudentTable.jsx
+++ EteneeFront/components/LuKStudentTable.jsx
@@ -58,13 +58,13 @@ class LuKStudentTable extends React.Component {
 
       const lukStudentData = [
       {lastName: 'Repomies', firstName: 'Rauno', studyPlan: 'LuK 2014', creditsAmount: 132, otherCredits: 22,
-      k1: 'X', k2: 'X', k3: 'X', k4: 'X', k5: 'X'}, 
+      k1: 'X', k2: 'X', k3: 'X', k4: 'X', k5: 'X'},
       {lastName: 'Jarla', firstName: 'Pertti', studyPlan: 'LuK 2014', creditsAmount: 145, otherCredits: 27,
-      k1: 'X', k2: '', k3: 'X', k4: 'X', k5: 'X'}, 
+      k1: 'X', k2: '', k3: 'X', k4: 'X', k5: 'X'},
       {lastName: 'Hedberg', firstName: 'Henrik', studyPlan: 'LuK 2014', creditsAmount: 140, otherCredits: 25,
-      k1: 'X', k2: 'X', k3: 'X', k4: 'X', k5: ''}, 
+      k1: 'X', k2: 'X', k3: 'X', k4: 'X', k5: ''},
       {lastName: 'Pelkonen', firstName: 'Niko', studyPlan: 'LuK 2015', creditsAmount: 81, otherCredits: 12,
-      k1: '', k2: 'X', k3: 'X', k4: 'X', k5: ''}, 
+      k1: '', k2: 'X', k3: 'X', k4: 'X', k5: ''},
       {lastName: 'Niiranen', firstName: 'Pekka', studyPlan: 'LuK 2015', creditsAmount: 92, otherCredits: 11,
       k1: 'X', k2: 'X', k3: 'X', k4: '', k5: 'X'},
       {lastName: 'Kivijakola', firstName: 'Päivi', studyPlan: 'LuK 2015', creditsAmount: 82, otherCredits: 15,
@@ -74,13 +74,16 @@ class LuKStudentTable extends React.Component {
       {lastName: 'Meikäläinen', firstName: 'Matti', studyPlan: 'LuK 2016', creditsAmount: 30, otherCredits: 5,
       k1: 'X', k2: 'X', k3: 'X', k4: '', k5: 'X'},
       ]
-    
+
       return (
-        <ReactTable
-           data={lukStudentData}
-           columns={lukStudentColumns}
-           defaultPageSize={5}
-         />
+        <div>
+          <h3>Luk-opiskelijat</h3>
+          <ReactTable
+             data={lukStudentData}
+             columns={lukStudentColumns}
+             defaultPageSize={5}
+           />
+        </div>
       )
    }
 }
