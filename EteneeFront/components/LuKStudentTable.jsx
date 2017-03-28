28a7a6ce7e5b47b1d00de8eef2022e62a5c1a988
Pekka Niiranen <nihilpekka@gmail.com>
2017-03-28 21:06:39 +0300

    fixed column problem

--- EteneeFront/components/LuKStudentTable.jsx
+++ EteneeFront/components/LuKStudentTable.jsx
@@ -8,26 +8,12 @@ class LuKStudentTable extends React.Component {
     super(props);
     console.log(props);
     this.state = {
-      courses: props.curriculum
+      courses: []
     }
     console.log('state coming');
     console.log(this.state);
   }
-  componentDidMount() {
-    const curriculum = this.props.curriculum
-    console.log('curri');
-    console.log(curriculum);
-  }
-  componentDidUpdate(courses) {
-    console.log('r')
-    console.log(courses);
-  }
    render() {
-     const courses = [];
-
-     const curAr = this.props.curriculum;
-     console.log('curar');
-     console.log(curAr);
       const lukStudentColumns = [{
       columns: [{
         header: 'Sukunimi',
@@ -36,34 +22,23 @@ class LuKStudentTable extends React.Component {
       }, {
         header: 'Etunimi',
         accessor: 'firstName'
-      }]
-      }, {
-        columns: [{
-          render: row => (
-            this.props.curriculum.courses.map(course => <div>{course.name}</div>)
-          )
         }]
       }]
 
-      const lukStudentData = [
-      {lastName: 'Repomies', firstName: 'Rauno', studyPlan: 'LuK 2014', creditsAmount: 132, otherCredits: 22,
-      k1: 'X', k2: 'X', k3: 'X', k4: 'X', k5: 'X'},
-      {lastName: 'Jarla', firstName: 'Pertti', studyPlan: 'LuK 2014', creditsAmount: 145, otherCredits: 27,
-      k1: 'X', k2: '', k3: 'X', k4: 'X', k5: 'X'},
-      {lastName: 'Hedberg', firstName: 'Henrik', studyPlan: 'LuK 2014', creditsAmount: 140, otherCredits: 25,
-      k1: 'X', k2: 'X', k3: 'X', k4: 'X', k5: ''},
-      {lastName: 'Pelkonen', firstName: 'Niko', studyPlan: 'LuK 2015', creditsAmount: 81, otherCredits: 12,
-      k1: '', k2: 'X', k3: 'X', k4: 'X', k5: ''},
-      {lastName: 'Niiranen', firstName: 'Pekka', studyPlan: 'LuK 2015', creditsAmount: 92, otherCredits: 11,
-      k1: 'X', k2: 'X', k3: 'X', k4: '', k5: 'X'},
-      {lastName: 'Kivijakola', firstName: 'Päivi', studyPlan: 'LuK 2015', creditsAmount: 82, otherCredits: 15,
-      k1: 'X', k2: 'X', k3: '', k4: 'X', k5: ''},
-      {lastName: 'Niinistö', firstName: 'Sauli', studyPlan: 'LuK 2016', creditsAmount: 32, otherCredits: 0,
-      k1: 'X', k2: 'X', k3: 'X', k4: '', k5: 'X'},
-      {lastName: 'Meikäläinen', firstName: 'Matti', studyPlan: 'LuK 2016', creditsAmount: 30, otherCredits: 5,
-      k1: 'X', k2: 'X', k3: 'X', k4: '', k5: 'X'},
-      ]
-
+      const curriculum = this.props.curriculum;
+      try {
+        _.forEach(curriculum.courses, function(course) {
+          course.header = course.name;
+          course.id = course.code
+          course.accessor = course.name;
+          lukStudentColumns[0].columns.push(course)
+        })
+        console.log('got columns');
+        console.log(lukStudentColumns);
+      }
+      catch (error) {
+        //console.log(error);
+      }
       const lukArray = _.filter(this.props.students.students, {"curriculum": {"ops": "LUK"}})
       console.log('lukArray coming');
       console.log(this.props);
