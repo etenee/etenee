7869d7cd64efd8d75f9365e2a51ce8d9a31738b1
Pekka Niiranen <nihilpekka@gmail.com>
2017-03-30 10:07:51 +0300

    database done

---
+++ EteneeBack/scripts/conversion.js
@@ -0,0 +1,30 @@
+var sqlite3 = require('sqlite3').verbose()
+var db = new sqlite3.Database('.././models/database.db')
+var curriculumFm14 = require('.././data/curriculumFm14.json');
+var curriculumFm15 = require('.././data/curriculumFm15.json');
+var curriculumLuk14 = require('.././data/curriculumLuk14.json');
+var curriculumLuk15 = require('.././data/curriculumLuk15.json');
+
+let curriculumArray = [
+  {name: "curriculumFm14",
+    data: curriculumFm14},
+  {name: "curriculumFm15",
+    data: curriculumFm15},
+  {name: "curriculumLuk14",
+    data: curriculumLuk14},
+  {name: "curriculumLuk15",
+    data: curriculumLuk15}
+  ];
+
+  for (value of curriculumArray) {
+    let curriculumName = value.name;
+    let coursesArray = value.data.courses;
+    db.serialize(function() {
+      db.run('CREATE TABLE if not exists ' + value.name + '(credits integer, code varchar(20), name varchar(30))');
+    });
+
+    for (course of coursesArray) {
+      //db.run('INSERT INTO '+ curriculumName +' VALUES ('course.credits ',"'course.code'","'course.name'")');
+      db.run('INSERT INTO ' + curriculumName + ' VALUES ('+course.credits+',"'+course.code+'","'+course.name+'");' );
+    }
+  };
--- EteneeBack/server.js
+++ EteneeBack/server.js
@@ -2,11 +2,15 @@
 var express = require('express');
 var app = express();
 
+
 //import prototype dummy data
 var data = require ('./data/data.json');
 var fm = require ('./data/fmTrue.json');
 var all = require ('./data/all.json');
 var bachelorCurriculum = require ('./data/curriculumLuk14.json');
+var sqlite3 = require('sqlite3').verbose()
+var db = new sqlite3.Database('./models/database.db')
+//var database = require('./models/database.db');
 
 //assign port
 var port = process.env.PORT || 3001;
@@ -34,6 +38,13 @@ app.get('/students/', function(req, res) {
 
 app.get('/bachelorCurriculum/', function(req, res) {
 	res.json(bachelorCurriculum);
+});
+
+app.get('/db/', function(req, res) {
+  db.each('SELECT * from courses', function(err, row){
+    console.log(row.id + '' + row.code + '' + row.name);
+    res.json({id: row.id, code: row.code, name: row.name});
+  })
 });
 
 //server will listen to port 3001
