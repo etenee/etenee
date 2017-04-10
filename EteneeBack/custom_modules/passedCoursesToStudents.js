2095ccb722602ca3a754494a32bcfe7c3776fdc6
Pekka Niiranen <nihilpekka@gmail.com>
2017-04-10 19:58:52 +0300

    added passedCourses module for populating students endpoint json with passedCourses

---
+++ EteneeBack/custom_modules/passedCoursesToStudents.js
@@ -0,0 +1,27 @@
+var sqlite3 = require('sqlite3').verbose()
+var db = new sqlite3.Database('./models/database.db')
+const Promise = require('bluebird');
+const async = require('async');
+
+const passedCourses = function(list, call) {
+  const studentsList = list;
+  async.forEachOf(studentsList.students, function(object, key, callback){
+    if (studentsList.students[key].passedCourses === undefined) {
+      studentsList.students[key].passedCourses = [];
+    }
+    db.each('SELECT * FROM passedCourses WHERE studentId = '+studentsList.students[key].studentId,
+      function(err, row) {
+        console.log(studentsList.students[key].studentId);
+        studentsList.students[key].passedCourses.push(row);
+      },
+      function() {
+        callback();
+      });
+    },
+    function(done) {
+      //console.log(studentsList.students);
+      call(studentsList);
+    });
+  }
+
+  module.exports = passedCourses;
