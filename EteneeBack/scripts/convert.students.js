c69696e330dee5f532682728bbb76b3592330258
Pekka Niiranen <nihilpekka@gmail.com>
2017-04-10 19:59:32 +0300

    added curiculum field

--- EteneeBack/scripts/convert.students.js
+++ EteneeBack/scripts/convert.students.js
@@ -5,7 +5,7 @@ var all = require('.././data/all.json');
 
 db.serialize(function() {
   db.run('DROP TABLE IF EXISTS students;');
-  db.run('CREATE TABLE if not exists students (studentId integer PRIMARY KEY NOT NULL, firstName varchar(30), lastName varchar(30), totalCredits integer, otherCredits integer, instructorId integer);');
+  db.run('CREATE TABLE if not exists students (studentId integer PRIMARY KEY NOT NULL, firstName varchar(30), lastName varchar(30), totalCredits integer, otherCredits integer, instructorId integer, curriculum varchar(30));');
 });
 
 /*for (let student of all.students) {
@@ -13,5 +13,5 @@ db.serialize(function() {
 }*/
 
 for (let student of all.students) {
-  db.run('INSERT OR REPLACE INTO students VALUES ('+student.id+', "'+student.firstName+'", "'+student.lastName+'", '+student.totalCredits+', '+student.otherCredits+', '+student.instructorId+');');
+  db.run('INSERT OR REPLACE INTO students VALUES ('+student.id+', "'+student.firstName+'", "'+student.lastName+'", '+student.totalCredits+', '+student.otherCredits+', '+student.instructorId+',"'+student.curriculum.ops+'");');
 };
