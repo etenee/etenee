ed30a9349aa95bfbb682529f57892eb726510bcd
Pekka Niiranen <nihilpekka@gmail.com>
2017-04-21 11:27:53 +0300

    made repeater

---
+++ EteneeFront/components/repeater.jsx
@@ -0,0 +1,35 @@
+import React, { Component, PropTypes } from 'react'
+//import {connect} from 'react-redux'
+import ReactTable from 'react-table'
+import { ReactTableDefaults } from 'react-table'
+
+class Repeater extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+    render() {
+        /*let rows = this.props.map((row) => {
+            return <h1>row</h1>
+        }); */
+
+        /*
+        {this.props.group.map(function(curriculumName) {
+                        return <th key={curriculumName}>{curriculumName}</th>;
+                    })}
+        */ 
+
+        return (
+            <div>
+                <h2>
+                    
+                </h2>
+            </div>
+        )
+
+    }
+}
+
+export default Repeater;
